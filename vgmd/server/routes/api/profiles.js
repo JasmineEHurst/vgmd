132b7198a1cf376222624b609eb610aa498ab1be
Jasmine Hurst <jasmine.ehurst@gmail.com>
2018-04-28 14:58:00 -0700

    Added more Profile routes

--- vgmd/server/routes/api/profiles.js
+++ vgmd/server/routes/api/profiles.js
@@ -31,6 +31,63 @@ router.get(
   }
 );
 
+//@route  GET /api/profiles/handle/:handle
+//@desc   Get a user profile by their handle
+//@access Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      } else {
+        res.json(profile);
+      }
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route  GET /api/profiles/user/:user_id
+//@desc   Get a user profile by their user id
+//@access Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
+
+//@route  GET /api/profiles/all
+//@desc   Get all profiles
+//@access Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofiles = "There are no profiles";
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => {
+      res.status(404).json({ profile: "There are no profiles" });
+    });
+});
+
 //@route  POST /api/profiles
 //@desc   Create or edit user profile
 //@access Private
@@ -112,15 +169,6 @@ router.post(
       }
     });
   }
-);
-
-//@route  GET /api/profiles/test
-//@desc   Tests the profiles route
-//@access Public
-router.get("/test", (req, res) =>
-  res.json({
-    msg: "Profiles resource works"
-  })
 );
 
 // Have to export module so server.js can pick it up
